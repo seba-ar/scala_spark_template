da7e8b58a5bd419eddbcbc54769a4cd18fe1f51d
seba-ar <sebastian.nunez_externo@altanredes.com>
2025-06-17 17:27:59 -0600

    change move files to write processed files

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,5 @@
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{SparkSession}
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.streaming.StreamingQueryListener
 import org.apache.spark.sql.streaming.StreamingQueryListener.{
   QueryStartedEvent,
@@ -10,69 +11,16 @@ import org.apache.spark.sql.types._
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.streaming.Trigger
 import java.nio.file.{Files, Paths, StandardCopyOption}
-// import org.apache.spark.sql.hive.HiveContext
 
-import cdr.CdrData
+import cdr.{CdrData, CdrDataError}
 
 object ParseCDR {
+  // Set up logging
   Logger.getLogger("atlas").setLevel(Level.ERROR)
   Logger.getLogger("producer").setLevel(Level.ERROR)
   Logger.getLogger("org").setLevel(Level.WARN)
   val app_logger = Logger.getLogger("app")
   app_logger.setLevel(Level.DEBUG)
-
-  // Function to move files
-  def moveProcessedFiles(
-      filePaths: Array[String],
-      inputPath: String,
-      processedDirectory: String
-  ): Unit = {
-    if (filePaths.isEmpty) {
-      app_logger.info("No files to move in this batch.")
-      return
-    }
-
-    filePaths.foreach { filepath =>
-      val inputPathClean = inputPath.replace("file://", "")
-      val processedDirectoryClean = processedDirectory.replace("file://", "")
-
-      val originalFile = new java.io.File(s"${filepath}")
-      val filename = originalFile.getName
-
-      val destinationFile =
-        new java.io.File(s"${processedDirectoryClean}/${filename}")
-
-      if (originalFile.exists()) {
-        try {
-          // Ensure the processed directory exists
-          val processedDir = new java.io.File(processedDirectoryClean)
-          if (!processedDir.exists()) {
-            processedDir.mkdirs() // Create directories if they don't exist
-          }
-
-          if (originalFile.renameTo(destinationFile)) {
-            app_logger.info(
-              s"Successfully moved file: ${originalFile} to ${destinationFile}"
-            )
-          } else {
-            app_logger.error(
-              s"Failed to move file: ${originalFile} to ${destinationFile}"
-            )
-          }
-        } catch {
-          case e: Exception =>
-            app_logger.error(
-              s"Error moving file ${originalFile}: ${e.getMessage}",
-              e
-            )
-        }
-      } else {
-        app_logger.warn(
-          s"File not found at original path during move attempt: ${originalFile}"
-        )
-      }
-    }
-  }
 
   def main(args: Array[String]): Unit = {
 
@@ -91,6 +39,7 @@ object ParseCDR {
       sparkBuilder.getOrCreate()
     }
 
+    // customize Spark logging
     spark.streams.addListener(new StreamingQueryListener() {
       override def onQueryStarted(queryStarted: QueryStartedEvent): Unit = {
         println("Query started: " + queryStarted.id)
@@ -105,19 +54,23 @@ object ParseCDR {
       }
     })
 
+    // Set up input parameters with defaults
     val inputPath = args.lift(0).getOrElse("input")
-    val chkpoint = args.lift(1).getOrElse("chk-point-dir")
-    val outputPath = args.lift(2).getOrElse("output")
-    val processedPath = args.lift(3).getOrElse("processed")
-    val maxFiles = args.lift(4).map(_.toInt).getOrElse(1)
+    val inputMath = args.lift(1).getOrElse("/*/*.add")
+    val chkpoint = args.lift(2).getOrElse("chk-point-dir")
+    val outputPath = args.lift(3).getOrElse("output")
+    val outputErrPath = args.lift(4).getOrElse("outputErr")
+    val processedTable = args.lift(5).getOrElse("processed")
+    val maxFiles = args.lift(6).map(_.toInt).getOrElse(1)
 
+    // import cdrdata schema
     val dataSchema = CdrData.getBaseTypes
 
     val rawDF = spark.readStream
       .schema(dataSchema)
       .format("csv")
       .option("sep", "|")
-      .option("path", s"$inputPath/*/*.add")
+      .option("path", s"$inputPath$inputMath")
       .option("maxFilesPerTrigger", maxFiles) // test more options
       .load()
 
@@ -129,53 +82,54 @@ object ParseCDR {
       regexp_extract(col("filename"), ".*/(.*)", 1)
     )
 
-    val transformedDf = CdrData.getTransformations.foldLeft(df_cleanedfn) {
-      case (tempDf, (colName, transformation)) =>
-        tempDf.withColumn(colName, transformation)
-    }
-
-    val orderedDF = CdrData.selectOrder(transformedDf)
-
-    // app_logger.debug(transformedDf.schema)
-
-    // val testDF = orderedDF.select("cdr_id","cust_local_start_date", "filename")
-
-    // val consoleDF = testDF.writeStream
-    //   .outputMode("append")
-    //   .format("console")
-    //   .option("numRows", 2)
-    //   .option("truncate", "false")
-    //   .start()
-
-    // consoleDF.awaitTermination()
-
-    val write_parquet = orderedDF.writeStream
+    val write_parquet = df_cleanedfn.writeStream
       .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
-        val excludeRawFilename = batchDF.columns.filter(_ != "raw_filename")
-        val filteredDF = batchDF.select(excludeRawFilename.map(col): _*)
+        val transformedDf = CdrData.getTransformations.foldLeft(batchDF) {
+          case (tempDf, (colName, transformation)) =>
+            tempDf.withColumn(colName, transformation)
+        }
+        val selectOrderedDF = CdrData.selectOrder(transformedDf)
 
-        // only for tests
+        val transformErrDF = CdrDataError.getTransformations.foldLeft(batchDF) {
+          case (tempDf, (colName, transformation)) =>
+            tempDf.withColumn(colName, transformation)
+        }
+
+        val selectOrderErrDF = CdrDataError.selectOrder(transformErrDF)
+
         if (outputPath == "output") { //  TODO: BORRAR IF SOLO es para TESTsitos
-          filteredDF.write
+          selectOrderedDF.write
             .mode("append")
             .partitionBy("billing_month", "event_hour")
             .parquet(outputPath)
+
+          selectOrderErrDF.write
+            .mode("append")
+            .partitionBy("load_error_date")
+            .parquet(outputErrPath)
         } else {
-          filteredDF.write
+          selectOrderedDF.write
             .mode("append")
             .insertInto(outputPath)
+
+          selectOrderErrDF.write
+            .mode("append")
+            .insertInto(outputErrPath)
         }
 
-        val filePathsToMove = batchDF
-          .select("raw_filename")
-          .distinct()
-          .collect()
-          .map(row => s"${row.getString(0).replace("file:", "")}")
-
         app_logger.info(
-          s"Files to consider moving for batch ${filePathsToMove.length}"
+          s"Writing ${filePathsToMove.length} Files processed"
         )
-        moveProcessedFiles(filePathsToMove, inputPath, processedPath)
+
+        // insert files to processedTable with current timestamp
+        val filePathsToMove = batchDF
+          .withColumnRenmed("raw_filename", "filename")
+          .select("filename", current_timestamp().alias("load_date"))
+          .distinct()
+          .parquet(processedTable)
+        
+
+
       }
       .option("checkpointLocation", chkpoint)
       .start()
